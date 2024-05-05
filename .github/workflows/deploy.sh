f98bb4b182fca7ec3765303f9d5b80df6daba6ba
BranzSkuy <96824911+Brannz@users.noreply.github.com>
2024-05-05 12:49:16 +0700

    Delete .github/workflows directory

--- .github/workflows/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Definisikan variabel-variabel yang diperlukan
-REMOTE_HOST="ecs-user@47.236.31.81"
-REMOTE_DIR="/opt/lampp/github/"
-PASSWORD="Stiga4990l-"
-
-# Jalankan perintah untuk menyalin file ke server
-sshpass -p "$PASSWORD" scp -r ./opt/lampp/github/* $REMOTE_HOST:$REMOTE_DIR
